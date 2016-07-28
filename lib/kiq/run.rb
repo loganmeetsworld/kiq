b89565d25633cc13bc46072cb3515bb4f044fdaa
Logan McDonald <loganmcdona11@gmail.com>
2016-07-27 21:26:28 -0700

    fix typo in run

--- lib/kiq/run.rb
+++ lib/kiq/run.rb
@@ -98,7 +98,7 @@ module Kiq
       # If Project instance exists, lists all backers for a certain project
       # @return [String] of backers, or error and help if project does not exist
       def list(project_name)
-        if !Project.project_does_not_ exist?(['project', project_name], @projects)
+        if !Project.project_does_not_exist?(['project', project_name], @projects)
           project = Project.all_offspring.find { |p| p.name == project_name }
           puts "Project Name: #{project.name}"
           puts "Amount Remaining: $#{project.amount}"
