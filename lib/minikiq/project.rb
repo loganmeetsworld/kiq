838370fd023c92034a139676a5e5a96462eac8fd
Logan McDonald <loganmcdona11@gmail.com>
2016-07-27 18:23:18 -0700

    refactors project.rb documentation

--- lib/minikiq/project.rb
+++ lib/minikiq/project.rb
@@ -15,14 +15,15 @@ module Minikiq
       end
 
       # Sets up easy was to call all instances of an object
-      # Returns instances
+      # @return instances
       def self.all_offspring
         @@instance_collector
       end
 
-      # Takes a project [Array] and all Project instances [Project]
+      # @param project [Array] the user input for project
+      # @param projects [Array] all instances of Project
       # Calls all validation methods for a given project
-      # Returns boolean
+      # @return [Boolean]
       def self.validate_project(project, projects)
         type = project[0]
         name = project[1]
@@ -31,9 +32,10 @@ module Minikiq
         return self.check_input_length(project) && self.project_does_not_exist?(project, projects) && self.check_amount_dollar_sign(amount) && self.check_name_characters(name) && self.check_name_length(name)
       end
 
-      # Takes a project [Array] and all Project instances [Project]
+      # @param project [Array] the user input for project
+      # @param projects [Array] all instances of Project
       # Checks if project already exists
-      # Returns boolean and warning if false
+      # @return [Boolean] warning if false
       def self.project_does_not_exist?(project, projects)
         if projects[project[1]].nil?
           return true
@@ -43,9 +45,9 @@ module Minikiq
         end
       end
 
-      # Takes in amount [String]
+      # @param amount [String]
       # Checks if the amount includes dollar signs
-      # Returns boolean and warning if false
+      # @return [Boolean] warning if false
       def self.check_amount_dollar_sign(amount)
         if !amount.include?('$')
           return true
@@ -55,9 +57,9 @@ module Minikiq
         end
       end
 
-      # Takes in full user input [Array]
+      # @param input [Array] full user input
       # Checks if the array is the correct length for project input
-      # Returns boolean and warning if false
+      # @return [Boolean] warning if false
       def self.check_input_length(input)
         if input.length == 3
           return true
@@ -67,9 +69,9 @@ module Minikiq
         end
       end
 
-      # Takes in name [String]
-      # Checks if the amount includes dollar signs
-      # Returns boolean and warning if false
+      # @param name [String]
+      # Checks if the name has non-alphanumeric characters besides underscores and dashes
+      # @return [Boolean] warning if false
       def self.check_name_characters(name)
         if name.scan(/[^\w-]/).empty?
           return true
@@ -79,9 +81,9 @@ module Minikiq
         end
       end
 
-      # Takes in name [String]
+      # @param name [String]
       # Checks if the string is between 4 and 20 characters
-      # Returns boolean and warning if false
+      # @return [Boolean] warning if false
       def self.check_name_length(name)
         if name.length >= 4 && name.length <= 20
           return true
